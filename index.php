c226223d628930a4c154e9770ba5dc47efd7e7a4
Jerry Linghui Lyu <jerry.lvlh@gmail.com>
2019-10-29 11:23:13 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <body>
         <table align="center">
             <tr>
-                <td align="center"><image src="iphone-promotion2.png" /><font size="10" color="#FF0000"><br>iPhoneX买一送一</font></td>
+                <td align="center"><image src="huawei-promotion1.PNG" /><font size="10" color="#FF0000"><br>华为大促销</font></td>
             </tr>
         </table>
     </div>
